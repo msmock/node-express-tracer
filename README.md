d675d3cf4de1a2f066c1acb8b26048d9fc58d116
Martin Smock <martin_smock@me.com>
2025-11-26 15:44:30 +0100

    added timestamp

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # node-express-tracer
-trace http calls - the app expects http POST requests on .../https://node-express-tracer.onrender.com and provides the body an header data on GET request.
+trace http calls - the app expects http POST requests on https://node-express-tracer.onrender.com and provides the body an header data on GET request.
 
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+let latestRequest = null;
+
 router.get('/', function(req, res, next) {
     res.send(JSON.stringify(latestRequest));
 });
 
 router.post('/', function(req, res, next) {
 
+    let timestamp = new Date().toISOString();
+
     let headers = req.headers;
     let body = req.body;
-    let data = [headers, body];
+    let data = [timestamp, headers, body];
+
     latestRequest = data;
 
     let dataStr = JSON.stringify(data);
     console.log(dataStr);
 
-    res.send('respond with a resource');
+    res.send('received post request');
 });
 
 module.exports = router;
