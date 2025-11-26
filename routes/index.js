f425fd3d427bd59396afaaf708ca03f7fc159e42
Martin Smock <martin_smock@me.com>
2025-11-26 09:35:50 +0100

    simplify

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send(JSON.stringify(latestRequest));
+});
+
+router.post('/', function(req, res, next) {
+
+    let headers = req.headers;
+    let body = req.body;
+    let data = [headers, body];
+    latestRequest = data;
+
+    let dataStr = JSON.stringify(data);
+    console.log(dataStr);
+
+    res.send('respond with a resource');
 });
 
 module.exports = router;
