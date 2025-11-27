7c7b37c0f85ed962486338da308f270567275ab9
Martin Smock <martin_smock@me.com>
2025-11-27 14:11:44 +0100

    added body parser

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 const cookieParser = require('cookie-parser');
