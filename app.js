8761dcc0f6dcee238366ffed03da96245d9dab05
Martin Smock <martin_smock@me.com>
2025-11-27 14:08:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -15,8 +14,14 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const bodyParser = require('body-parser');
+app.use( bodyParser.json() );
 
 app.use('/', indexRouter);
 
